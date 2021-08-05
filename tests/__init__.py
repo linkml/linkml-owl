f9654e8a0e83f62392af14bb1bda4db2ea00bf41
cmungall <cjm@berkeleybop.org>
2021-08-05 08:58:35 -0700

    docs

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+import os
+import pprint
+
+MODEL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'model')
+INPUT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'inputs')
+OUTPUT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'output')
+JSONLD_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../jsonld')
+
+
