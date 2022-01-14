01756411129732482437883b925f7c02ab88b1f6
Sujay Patil <sujaysanjeev.patil@gmail.com>
2022-01-13 17:03:07 -0800

    Github Actions to run test suite and auto publish to pypi

---
+++ linkml_owl/__init__.py
@@ -0,0 +1,7 @@
+# use importlib.metadata to read the version provided
+# by the package during installation. Do not hardcode
+# the version in the code
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
